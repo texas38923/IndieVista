72ba6191069c7068450f18673b1f26e3f6865dc4
TeqArine <67150797+texas38923@users.noreply.github.com>
2023-12-31 20:56:45 +0530

    Update about.js

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,5 +1,5 @@
 import Hero from '../components/Hero';
-import Navbar from './../components/Navbar';
+import Navbar from './../components/navbar';
 import AboutImg from '../assets/abt.jpg';
 import Footer from '../components/footer';
 import AboutUs from '../components/AboutUs';
