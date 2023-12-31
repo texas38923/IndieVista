5ba02474fb054ece5a42e2ea4518d4a024390e0e
TeqArine <67150797+texas38923@users.noreply.github.com>
2023-12-31 20:57:04 +0530

    Update home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,5 @@
 import Hero from '../components/Hero';
-import Navbar from './../components/Navbar';
+import Navbar from './../components/navbar';
 import HomeImg from '../assets/homepage.jpg';
 import Destination from '../components/Destination';
 import Footer from '../components/footer';
