7ce4b0ef778ef768735bf6dd54f608d76b7e04a7
TeqArine <67150797+texas38923@users.noreply.github.com>
2023-12-31 20:57:13 +0530

    Update service.js

--- src/routes/service.js
+++ src/routes/service.js
@@ -1,5 +1,5 @@
 import Hero from '../components/Hero';
-import Navbar from './../components/Navbar';
+import Navbar from './../components/navbar';
 import ServiceImg from '../assets/1.jpg';
 import Footer from '../components/footer';
 import Trip from '../components/trip';
